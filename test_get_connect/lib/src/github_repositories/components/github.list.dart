6f909e8f8e20250def0cfa7f18872d8271e1e404
Diego Alves Openheimer <diegoalvesopenheimer@MacBook-Pro-de-ICCSW-027333-NB.local>
2020-12-31 21:49:44 -0300

    Adicionado mensagem caso não seja encontrado nenhum repositório para o nome selecionado

--- test_get_connect/lib/src/github_repositories/components/github.list.dart
+++ test_get_connect/lib/src/github_repositories/components/github.list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:test_get_connect/src/github_repositories/model/repository.dart';
 
 typedef OnPress = void Function(Repository);
@@ -14,12 +15,22 @@ class GitHubListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoScrollbar(
-      child: ListView.builder(
-        itemCount: repositories.length,
-        itemBuilder: (BuildContext context, int index) {
-          return _item(repositories[index]);
-        },
-      ),
+      child: repositories.isNotEmpty ?  _buildListView() : _emptyMessage(),
+    );
+  }
+
+  Widget _emptyMessage() {
+    return Center(
+      child: Text('Without inforation...'),
+    );
+  }
+
+  ListView _buildListView() {
+    return ListView.builder(
+      itemCount: repositories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _item(repositories[index]);
+      },
     );
   }
 
